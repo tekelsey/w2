cf05f008efc804552e9f2cd9e85f034f54b87135
tekelsey <tekelsey@gmail.com>
2023-03-18 11:52:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-const words = ['apple', 'banana', 'cherry', 'grape', 'kiwi', 'lemon', 'mango', 'orange', 'peach', 'strawberry'];
-let currentWord = '';
-let scrambledWord = '';
-
-document.getElementById('submit-guess').addEventListener('click', checkGuess);
-document.getElementById('reset-game').addEventListener('click', resetGame);
-
-function scrambleWord(word) {
-    const characters = word.split('');
-    for (let i = characters.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [characters[i], characters[j]] = [characters[j], characters[i]];
-    }
-    return characters.join('');
-}
-
-function newGame() {
-    currentWord = words[Math.floor(Math.random() * words.length)];
-    scrambledWord = scrambleWord(currentWord);
-    document.getElementById('scrambled-word').textContent = scrambledWord;
-}
-
 function checkGuess() {
     const userInput = document.getElementById('user-input').value;
     const feedback = document.getElementById('feedback');
@@ -29,4 +7,5 @@ function checkGuess() {
     } else {
         feedback.textContent = 'Sorry, that is incorrect. Try again!';
         feedback.style.color = 'red';
-   
+    }
+} // This closing brace and parenthesis were missing
